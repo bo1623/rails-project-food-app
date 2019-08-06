25ec7f4b01ec7447942c7c009c77da4febb48dac
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-06 16:20:20 +0800

    Setting up restaurant_params to accommodate nested attributes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,35 @@
 class RestaurantsController < ApplicationController
 
-  def index
+  def index #build in filter later on
     # if params[:location_id]
     #   @restaurants = Restaurant.where(location_id: params[:location_id])
     # else
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant=Restaurant.find(params[:id])
+  end
 
+  def new
+    @restaurant = Restaurant.new
+    @restaurant.meals.build
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      redirect_to new_restaurant_path
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :corporation_id,:location_id,:opening_time,:closing_time,:revenue,:rating, meal_ids:[], meals_attributes: [:name,:price,:restaurant_id,:user_id,:rating])
+  end
 
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,8 +3,9 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :meals
   belongs_to :corporation
   belongs_to :location
+  accepts_nested_attributes_for :meals
 
-  def cuisine
+  def cuisine #for the show page
     self.corporation.cuisine
   end
 
