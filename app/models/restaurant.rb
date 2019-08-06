cc810634af01cca7972b85325b54bf65062d6557
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-06 15:55:44 +0800

    Added #cuisine getter method to restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,9 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :meals
   belongs_to :corporation
   belongs_to :location
+
+  def cuisine
+    self.corporation.cuisine
+  end
+
 end
