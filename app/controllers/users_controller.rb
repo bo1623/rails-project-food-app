6ae912ff9932ba341a02cc5c1423adfe23a2ed3d
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-07 14:46:47 +0800

    Added optional feature to user's belong_to restaurant feature, so that restaurant manager users can be saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    byebug
     if @user.save
       session[:user_id]=@user.id
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  belongs_to :restaurant
+  belongs_to :restaurant, optional: true
   has_many :meals
   has_many :dishes, through: :meals
   # has_many :restaurants, through: :dishes
