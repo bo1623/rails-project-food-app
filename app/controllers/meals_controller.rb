08cbdbfd31633a1eb80c0bba35a787721fdec158
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-07 17:50:35 +0800

    Added feature to see meal basket on meal show page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,6 +3,7 @@ class MealsController < ApplicationController
   def show
     @meal=Meal.find(params[:id])
     @dishes=@meal.dishes
+    @dish_names=@dishes.map{|dish| dish.name}.uniq
   end
 
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,10 @@ class Meal < ApplicationRecord
   belongs_to :user
   has_many :meals_dishes
   has_many :dishes, through: :meals_dishes
+
+  def total
+    dishes=self.dishes
+    dishes.map{|dish| dish.price}.sum
+  end
+
 end
