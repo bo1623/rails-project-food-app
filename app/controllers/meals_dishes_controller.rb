0e964e07f6b5aeb81a2b3fa3c4e60834f5a9b756
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-07 17:21:08 +0800

    Enabled form in show restaurant to handle multiple quantities of the same dish

--- app/controllers/meals_dishes_controller.rb
+++ app/controllers/meals_dishes_controller.rb
@@ -4,19 +4,13 @@ class MealsDishesController < ApplicationController
   def create
     @meal=Meal.new
     @user.meals << @meal
-    @meals_dish = MealsDish.new(meals_dishes_params)
-    @meals_dish.meal = @meal
-    if @meals_dish.save
-      redirect_to meal_path(@meal)
-    else
-      redirect_back
+    params[:dish_ids].zip(params[:dish_quantities]).each do |id,quantity|
+      quantity.to_i.times do #creating quantity times of meals_dish instances
+        MealsDish.create(meal_id: @meal.id, dish_id: id)
+      end
     end
-  end
-
-  private
-
-  def meals_dishes_params
-    params.permit(:meal_id,:dish_id,:meal_dish_rating)
+    redirect_to meal_path(@meal)
+    # redirect_back
   end
 
 
