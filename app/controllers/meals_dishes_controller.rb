68b7b9d87c1092c34cb11b4f68ddab62722e2f92
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-07 22:48:43 +0800

    Validation feature in meals_dishes controller create action - if all order quantities are 0 the user is redirected back to the restaurant show page where the order was place

--- app/controllers/meals_dishes_controller.rb
+++ app/controllers/meals_dishes_controller.rb
@@ -2,14 +2,18 @@ class MealsDishesController < ApplicationController
   before_action :current_user
 
   def create
-    @meal=Meal.new
-    @user.meals << @meal
-    params[:dish_ids].zip(params[:dish_quantities]).each do |id,quantity|
-      quantity.to_i.times do #creating quantity times of meals_dish instances
-        MealsDish.create(meal_id: @meal.id, dish_id: id)
+    if params[:dish_quantities].uniq == ['0']
+      redirect_back(fallback_location: root_path) #redirects back to previous page (restaurant show page)
+    else
+      @meal=Meal.new
+      @user.meals << @meal
+      params[:dish_ids].zip(params[:dish_quantities]).each do |id,quantity|
+        quantity.to_i.times do #creating quantity times of meals_dish instances
+          MealsDish.create(meal_id: @meal.id, dish_id: id)
+        end
       end
+      redirect_to meal_path(@meal)
     end
-    redirect_to meal_path(@meal)
   end
 
   def edit
