39960fde2223537686b2c4b53048048f5d61b0ba
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-08 00:46:28 +0800

    Completed update function in meals controller to feed ratings to associated meals_dishes instances

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,25 +1,43 @@
 class MealsController < ApplicationController
+  before_action :current_meal
 
   def show
-    @meal=Meal.find(params[:id])
     @dishes=@meal.dishes
     @dish_names=@dishes.map{|dish| dish.name}.uniq
   end
 
   def edit
-    @meal = Meal.find(params[:id])
   end
 
   def update
-    raise params.inspect
+    params[:names].zip(params[:dish_ratings]).each do |name, rating|
+      meals_dishes=MealsDish.where(meal_id: @meal.id, dish_id: Dish.find_by(name: name).id)
+      meals_dishes.each do |meals_dish|
+        meals_dish.meal_dish_rating=rating
+        meals_dish.save
+      end
+    end
+    redirect_to root_path
   end
 
+  # params hash from meals/edit.html.erb
+  # {"dish_ratings"=>["4", "3"],
+  #   "names"=>["Green Tea Latte", "Flat White"],
+  #   "commit"=>"Submit Ratings",
+  #   "controller"=>"meals", "action"=>"update",
+  #   "id"=>"16"}
+
   def destroy
-    @meal=Meal.find(params[:id])
     @restaurant=@meal.dishes[0].restaurant
     @meal.destroy
 
     redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def current_meal
+    @meal=Meal.find(params[:id])
   end
 
 
