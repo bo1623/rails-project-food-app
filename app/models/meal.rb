6d5550737bd8301dc91dd847ded89a190974c308
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-07 22:18:59 +0800

    Refactored meal show page with instance methods for @meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -8,4 +8,18 @@ class Meal < ApplicationRecord
     dishes.map{|dish| dish.price}.sum
   end
 
+  def dish_price(dish_name)
+    dishes=self.dishes.where(name: dish_name)
+    dishes[0].price
+  end
+
+  def dish_count(dish_name)
+    dishes=self.dishes.where(name: dish_name).count
+  end
+
+  def dishes_cost(dish_name)
+    dish_price(dish_name)*dish_count(dish_name)
+  end
+
+
 end
