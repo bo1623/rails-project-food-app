4282c0c1eb73fda2d5619ea86563c2fc78534a0a
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-08 15:29:09 +0800

    Commented out create and edit actions in MealsDishes Controller and removed form from restaurant show page

--- app/controllers/meals_dishes_controller.rb
+++ app/controllers/meals_dishes_controller.rb
@@ -1,23 +1,23 @@
 class MealsDishesController < ApplicationController
-  before_action :current_user
-
-  def create
-    if params[:dish_quantities].uniq == ['0'] #condition where all dish_quantites are 0
-      redirect_back(fallback_location: root_path) #redirects back to previous page (restaurant show page)
-    else
-      @meal=Meal.new
-      @user.meals << @meal
-      params[:dish_ids].zip(params[:dish_quantities]).each do |id,quantity|
-        quantity.to_i.times do #creating quantity times of meals_dish instances
-          MealsDish.create(meal_id: @meal.id, dish_id: id)
-        end
-      end
-      redirect_to meal_path(@meal)
-    end
-  end
-
-  def edit
-  end
+  # before_action :current_user
+  #
+  # def create
+  #   if params[:dish_quantities].uniq == ['0'] #condition where all dish_quantites are 0
+  #     redirect_back(fallback_location: root_path) #redirects back to previous page (restaurant show page)
+  #   else
+  #     @meal=Meal.new
+  #     @user.meals << @meal
+  #     params[:dish_ids].zip(params[:dish_quantities]).each do |id,quantity|
+  #       quantity.to_i.times do #creating quantity times of meals_dish instances
+  #         MealsDish.create(meal_id: @meal.id, dish_id: id)
+  #       end
+  #     end
+  #     redirect_to meal_path(@meal)
+  #   end
+  # end
+  #
+  # def edit
+  # end
 
 
 end
