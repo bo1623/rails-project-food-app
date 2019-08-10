0d385393c948bba4809a8cd75b8a2bd91635bd94
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-10 23:30:42 +0900

    Changed table style and only enable normal users to make food orders

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
 
   def home
-    @restaurants=Restaurant.all
+    Restaurant.order(restaurant_rating: :desc)
+    @restaurants=Restaurant.all[0...4]
   end
 
 end
